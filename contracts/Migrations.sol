8d8a1492536940922e7bcc4a112bc59ea7db3568
Chan, Danny <danny50610@yahoo.com.tw>
2018-06-18 17:25:47 +0800

    Update contracts

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,23 +1,24 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.23;
+
 
 contract Migrations {
     address public owner;
-    uint public last_completed_migration;
+    uint public lastCompletedMigration;
+
+    constructor() public {
+        owner = msg.sender;
+    }
 
     modifier restricted() {
         if (msg.sender == owner) _;
     }
 
-    function Migrations() {
-        owner = msg.sender;
+    function setCompleted(uint completed) public restricted {
+        lastCompletedMigration = completed;
     }
 
-    function setCompleted(uint completed) restricted {
-        last_completed_migration = completed;
-    }
-
-    function upgrade(address new_address) restricted {
-        Migrations upgraded = Migrations(new_address);
-        upgraded.setCompleted(last_completed_migration);
+    function upgrade(address newAddress) public restricted {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(lastCompletedMigration);
     }
 }
