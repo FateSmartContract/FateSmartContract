1bd5f48d3088c65e605c4bb4a96767a736b28225
Chan, Danny <danny50610@yahoo.com.tw>
2018-06-27 00:44:01 +0800

    Set active class

--- src/components/ServantAndCraftEssence/index.vue
+++ src/components/ServantAndCraftEssence/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card">
         <div class="card-body">
-            <router-link class="btn btn-primary" to="/dashboard/servant-and-craft-essence/servant">　從者　</router-link>
-            <router-link class="btn btn-primary" to="/dashboard/servant-and-craft-essence/craft-essence">概念裡裝</router-link>
+            <router-link class="btn btn-primary" active-class="active" to="/dashboard/servant-and-craft-essence/servant">　從者　</router-link>
+            <router-link class="btn btn-primary" active-class="active" to="/dashboard/servant-and-craft-essence/craft-essence">概念裡裝</router-link>
             <router-view class="mt-1"></router-view>
         </div>
     </div>
