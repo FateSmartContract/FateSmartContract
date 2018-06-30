0ce278dd41875117c1fc4fc8652d413816e57bf2
Chan, Danny <danny50610@yahoo.com.tw>
2018-06-30 16:17:45 +0800

    調整召喚介面

--- src/components/Dashboard/Summon.vue
+++ src/components/Dashboard/Summon.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="card summon-card">
-        <div class="card-body">
-            <div style="height: 60vh">
+        <div class="card-body" style="position: relative;">
+            <div style="">
                 <h1 class="text-center display-1 summon-title">卡池 001</h1>
             </div>
-            <div class="text-center">
+            <div class="text-center summon-button-div">
                 <div style="display: inline-block">
-                    <button type="button" class="btn btn-primary btn-lg" @click="summonOne()">
+                    <button type="button" class="btn btn-primary btn-lg btn-summon" @click="summonOne()">
                         召喚 1 回
                     </button>
                     <br/>
                     <span><i class="fas fa-cube"></i> 消耗 方塊 * 3</span>
                 </div>
-                <div style="display: inline-block; margin-left: 10vw">
-                    <button type="button" class="btn btn-primary btn-lg" @click="summonTen()">
+                <div style="display: inline-block; margin-left: 25%">
+                    <button type="button" class="btn btn-primary btn-lg btn-summon" @click="summonTen()">
                         召喚 10 回
                     </button>
                     <br/>
@@ -51,4 +51,18 @@
         text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
     }
 
+    .btn-summon {
+        padding: 18px 28px;
+        font-size: 22px;
+        line-height: normal;
+    }
+
+    .summon-button-div {
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        bottom: 10%;
+    }
 </style>
