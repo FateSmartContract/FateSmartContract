180f43c7c610b606a5ffdae680c967be8bbe30f9
Chan, Danny <danny50610@yahoo.com.tw>
2018-07-01 23:46:44 +0800

    修正卡片顯示高度

--- src/components/Dashboard/ServantAndCraftEssence/CraftEssenceList.vue
+++ src/components/Dashboard/ServantAndCraftEssence/CraftEssenceList.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="row" style="overflow-y: scroll; height: calc(100% - 40px);">
-        <div v-if="craftEssence !== null" v-for="id in craftEssence" class="col-sm-2 fgo-card">
-            <!--<img src="https://kazemai.github.io/fgo-vz/common/images/Servant/403000_status_servant_1.png" class="img-fluid img-thumbnail"/>-->
-            <img v-holder="'img=256x256?auto=yes&theme=vue&size=24&text=' + id.toString().padStart(3, 0)" class="img-fluid img-thumbnail">
+    <div style="overflow-y: scroll; height: calc(100% - 40px);">
+        <div v-if="craftEssence !== null" class="container">
+            <div class="row">
+                <div v-for="id in craftEssence" class="col-sm-2 fgo-card">
+                    <!--<img src="https://kazemai.github.io/fgo-vz/common/images/Servant/403000_status_servant_1.png" class="img-fluid img-thumbnail"/>-->
+                    <img v-holder="'img=256x256?auto=yes&theme=vue&size=24&text=' + id.toString().padStart(3, 0)" class="img-fluid img-thumbnail">
+                </div>
+            </div>
         </div>
-        <div v-if="craftEssence === null" class="col-12 text-center">
+        <div v-else class="text-center">
             <i class="fas fa-spinner fa-spin fa-3x"></i>
         </div>
     </div>
