12f6a367e57710e9820fcb4def0a6c97eb1c091c
Chan, Danny <danny50610@yahoo.com.tw>
2018-07-01 23:10:52 +0800

    完成抽卡動畫

--- src/components/Dashboard/Summon.vue
+++ src/components/Dashboard/Summon.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="card summon-card" v-if="!summoning" style="height: inherit">
+        <div v-if="!summoning" class="card summon-card" style="height: inherit">
             <div class="card-body" style="position: relative;">
                 <div style="">
                     <h1 class="text-center display-1 summon-title">卡池 001</h1>
@@ -23,15 +23,23 @@
                 </div>
             </div>
         </div>
-        <div class="card summon-card" v-if="summoning" style="height: inherit" @click="closeSummoningPage()">
+        <div v-else class="card summon-card" style="height: inherit" @click="closeSummoningPage()">
             <div class="card-body d-flex" style="position: relative;">
                 <div class="container" style="position: absolute; top: 50%; left: 0; transform: translate(0, -50%);">
                     <div class="row justify-content-center">
                         <div class="col-md-2 col-card" v-for="i in summonAmount" style="position: relative;">
-                            <img v-holder="'img=256x256?auto=yes&theme=vue&text=%20'" class="img-fluid img-thumbnail">
-                            <span class="fa-center">
-                                <i class="fas fa-spinner fa-pulse fa-4x"></i>
-                            </span>
+                            <div v-if="canShowCard(i - 1)">
+                                <img src="../../assets/card-back.png" class="img-fluid img-thumbnail ">
+                                <span class="fa-center">
+                                    <i class="fas fa-spinner fa-pulse fa-4x"></i>
+                                </span>
+                            </div>
+                            <div v-else>
+                                <img src="../../assets/card-background.png" class="img-fluid img-thumbnail">
+                                <span class="fa-center" style="color: white; font-size: 24px;">
+                                    {{ summonResult[i - 1] == null ? '000' : summonResult[i - 1].toString().padStart(3, 0) }}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -49,6 +57,13 @@
 
     export default {
         name: 'Summon',
+        data () {
+            return {
+                timer: undefined,
+                showIndex: 0,
+                showCard: []
+            }
+        },
         computed: {
             ...mapGetters('game', [
                 'summoning',
@@ -56,19 +71,42 @@
                 'summonResult'
             ])
         },
+        watch: {
+            summonResult: function (result) {
+                if (result[0] !== null) {
+                    this.timer = setInterval(() => {
+                        this.$set(this.showCard, this.showIndex, true)
+                        this.showIndex += 1
+                        if (!(this.showIndex < this.summonAmount)) {
+                            clearInterval(this.timer)
+                        }
+                    }, 500)
+                }
+            }
+        },
         methods: {
             summonOne: function () {
                 player.summonOne().then((result) => {
+                    this.showIndex = 0
+                    this.showCard = [false]
                     this.$store.commit('game/beginSummoning', 1)
                 })
             },
             summonTen: function () {
                 player.summonTen().then((result) => {
+                    this.showIndex = 0
+                    this.showCard = []
+                    for (let i = 0; i < 10; i++) {
+                        this.showCard.push(false)
+                    }
                     this.$store.commit('game/beginSummoning', 10)
                 })
             },
             closeSummoningPage: function () {
                 this.$store.commit('game/endSummoning')
+            },
+            canShowCard: function (index) {
+                return this.summonResult[index] === null || !this.showCard[index]
             }
         }
     }
