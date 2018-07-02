5d69b34150d0d74feed39090b89d1d11afa66aa3
Chan, Danny <danny50610@yahoo.com.tw>
2018-07-02 18:30:34 +0800

    Try fix unit test

--- test/unit/specs/Index.spec.js
+++ test/unit/specs/Index.spec.js
@@ -3,10 +3,10 @@ import Index from '@/components/Index'
 import router from '@/router'
 
 describe('Index.vue', () => {
-    it('should render correct contents', () => {
+    it('Should render correct title', () => {
         const Constructor = Vue.extend(Index)
         const vm = new Constructor({router}).$mount()
-        expect(vm.$el.querySelector('.display-4 h1').textContent)
+        expect(vm.$el.querySelector('h1.display-4').textContent)
             .to.equal('Fate/Smart Contract')
     })
 })
